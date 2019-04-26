9022a8629dd22fef7ad0c01f07a5e4b2549c3be2
Gavin Jin <gavinjin62@gmail.com>
2019-04-25 22:14:22 -0700

    add welcome, items route

--- client/src/routes/Layout.js
+++ client/src/routes/Layout.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
+import ItemsContainer from '../pages/Items';
+import Home from '../pages/Home';
 
 export default () => (
   <Fragment>
@@ -13,6 +15,11 @@ export default () => (
        * Later, we'll add logic to send users to one set of routes if they're logged in,
        * or only view the /welcome page if they are not.
        */}
+      <Route path="/items" component={ItemsContainer} />
+      {/* <Route exact path="/profile" component={Profile} /> */}
+      <Route path="/welcome" component={Home} />
+      {/* <Route path={`${match.url}`} component={Profile} /> */}
+      {/* <Route path="/share" component={Share} /> */}
     </Switch>
   </Fragment>
 );

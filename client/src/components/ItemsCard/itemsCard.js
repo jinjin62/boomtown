157d0abd08173b7873755b55ab2cc6a0062daea1
Gavin Jin <gavinjin62@gmail.com>
2019-04-29 14:23:43 -0700

    install final form

--- client/src/components/ItemsCard/itemsCard.js
+++ client/src/components/ItemsCard/itemsCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
-
 import Button from '@material-ui/core/Button';
-
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import {Avatar} from '@material-ui/core';
+
+import { Avatar, Grid } from '@material-ui/core';
+
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './style';
-import { Grid } from '@material-ui/core';
+
 import Gravatar from 'react-gravatar';
 
 function ItemsCard({ classes, item }) {
@@ -24,22 +24,24 @@ function ItemsCard({ classes, item }) {
         />
         <CardContent className={classes.cardContent}>
           <div>
-            <Avatar >
+            <Avatar>
               {item.itemowner && <Gravatar email={item.itemowner.email} />}
             </Avatar>
           </div>
           <Typography gutterBottom variant="h5" component="h2">
             {item.title}
           </Typography>
-          {/* <Typography>{item.tags.map(tag => tag.title).join(', ')}</Typography> */}
+          <Typography>{item.tags.map(tag => tag.title)}</Typography>
           <Typography>{item.description}</Typography>
         </CardContent>
         <CardActions>
-          <Button size="small" color="primary">
-            View
-          </Button>
-          <Button size="small" color="primary">
-            Edit
+          <Button
+            size="medium"
+            variant="outlined"
+            color="default"
+            className={classes.button}
+          >
+            Borrow Item
           </Button>
         </CardActions>
       </Card>

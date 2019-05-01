2fae0021dee514d5bf875e2f25861cec66e452f0
Gavin Jin <gavinjin62@gmail.com>
2019-04-30 22:25:28 -0700

    pass tag prop to itemscard

---
+++ client/src/redux/ShareItemPreview/reducer.js
@@ -0,0 +1,45 @@
+// actions
+const UPDATE_ITEM = 'UPDATE_ITEM';
+
+const RESET_IMAGE = 'RESET_IMAGE';
+
+const RESET_ITEM = 'RESET_ITEM';
+
+//action creators
+export const updateItem = item => ({
+  type: UPDATE_ITEM,
+  payload: item
+});
+export const resetImage = () => ({
+  type: RESET_IMAGE
+});
+export const resetItem = () => ({
+  type: RESET_ITEM
+});
+
+// initial state
+const initialState = {
+  title: 'Name your item title',
+  description: 'Describe your item description',
+  imageurl: 'https://via.placeholder.com/350x250.png?text=select+an+image',
+  tags: [],
+  itemowner: {},
+  created: new Date()
+};
+
+//reducer
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case UPDATE_ITEM: {
+      return { ...state, ...action.payload };
+    }
+    case RESET_ITEM: {
+      return { ...initialState };
+    }
+    case RESET_IMAGE: {
+      return { ...state, imageurl: initialState.imageurl };
+    }
+    default:
+      return state;
+  }
+};

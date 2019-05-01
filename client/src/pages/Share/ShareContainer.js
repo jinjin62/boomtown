583613f832d3c85ed24baa450700c9b078776338
Gavin Jin <gavinjin62@gmail.com>
2019-04-30 22:33:44 -0700

    add tags query

--- client/src/pages/Share/ShareContainer.js
+++ client/src/pages/Share/ShareContainer.js
@@ -3,13 +3,22 @@ import Share from './Share';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
 // import FullScreenLoader from '../../components/FullScreenLoader';
-// import { Query } from 'react-apollo';
+import { Query } from 'react-apollo';
+import { ALL_TAGS_QUERY } from '../../apollo/queries';
 // import { } from '../../apollo/queries';
 
 class ShareContainer extends Component {
   render() {
     return (
-      <Share />
+      <Query query={ALL_TAGS_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...';
+          if (error) return `Error! ${error.message}`;
+          if (data) {
+            return <Share tags={data.tags} />;
+          }
+        }}
+      </Query>
     );
   }
 }

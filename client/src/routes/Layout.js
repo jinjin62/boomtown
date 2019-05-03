3ed5851d5b869e47a20c347dc1f9a5553592830a
Gavin Jin <gavinjin62@gmail.com>
2019-05-02 21:30:43 -0700

    add react loader

--- client/src/routes/Layout.js
+++ client/src/routes/Layout.js
@@ -1,20 +1,45 @@
-import React, { Fragment } from 'react';
-import { Redirect, Route, Switch } from 'react-router';
-import ItemsContainer from '../pages/Items';
+import React from 'react';
+import { Route, Redirect, Switch } from 'react-router';
 import Home from '../pages/Home';
-import AppBar from '../components/AppBar';
+import Items from '../pages/Items';
+import Profile from '../pages/Profile';
 import Share from '../pages/Share';
 
+import { ViewerContext } from '../context/ViewerProvider';
+import FullScreenLoader from '../components/FullScreenLoader';
+import AppBar from '../components/AppBar';
+// import PRoute from '../components/PrivateRoute';
+
 export default () => (
-  <Fragment>
-    {/* @TODO: Add your menu component here */}
-    <Route component={AppBar} />
-    <Switch>
-      <Route exact path="/items" component={ItemsContainer} />
-      {/* <Route exact path="/profile" component={Profile} />
-      <Route exact path="/profile/:userid" component={Profile} /> */}
-      <Route exact path="/share" component={Share} />
-      <Redirect from="*" to="/items" />
-    </Switch>
-  </Fragment>
+  <ViewerContext.Consumer>
+    {({ loading, viewer }) => {
+      // <FullScreenLoader />
+      if (loading) return <FullScreenLoader />;
+      if (!viewer) {
+        return (
+          <Switch>
+            <Route path="/welcome" component={Home} />
+            <Redirect to="/welcome" />
+          </Switch>
+        );
+      }
+      return (
+        <React.Fragment>
+          <AppBar />
+          <Switch>
+            <Route exact path="/items" name="items" component={Items} />
+            <Route exact path="/profile" name="profile" component={Profile} />
+            <Route
+              exact
+              path="/profile/:userId"
+              name="profile"
+              component={Profile}
+            />
+            <Route exact path="/share" name="share" component={Share} />
+            <Redirect from="*" to="/items" />
+          </Switch>
+        </React.Fragment>
+      );
+    }}
+  </ViewerContext.Consumer>
 );

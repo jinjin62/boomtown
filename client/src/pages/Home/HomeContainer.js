75f8f8cb803ae2dc43b613d793b95756b579c9d0
Gavin Jin <gavinjin62@gmail.com>
2019-05-02 21:38:42 -0700

    remove test query

--- client/src/pages/Home/HomeContainer.js
+++ client/src/pages/Home/HomeContainer.js
@@ -2,27 +2,10 @@ import React, { Component } from 'react';
 import Home from './Home';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-import { Query } from 'react-apollo';
-import {
-  ALL_TAGS_QUERY,
-  ITEM_QUERY,
-  ALL_ITEMS_QUERY
-} from '../../apollo/queries';
 
 class HomeContainer extends Component {
   render() {
-    return (
-      <Query query={ALL_ITEMS_QUERY} variables={{ filter: 2 }}>
-        {({ loading, error, data }) => {
-          if (loading) return 'Loading...';
-          if (error) return `Error! ${error.message}`;
-          if (data) {
-            console.log(data);
-            return <Home classes={this.props.classes} />;
-          }
-        }}
-      </Query>
-    );
+    return <Home classes={this.props.classes} />;
   }
 }
 export default withStyles(styles)(HomeContainer);

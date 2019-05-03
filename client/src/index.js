11a8899221fa700fb2f531bb556616e1909fb028
Gavin Jin <gavinjin62@gmail.com>
2019-05-02 21:35:20 -0700

    add viewer provider

--- client/src/index.js
+++ client/src/index.js
@@ -12,52 +12,7 @@ import registerServiceWorker from './registerServiceWorker';
 import theme from './theme';
 import Layout from './routes/Layout';
 
-/**
- * @TODO: Initialize Apollo Client
- *
- * Uncomment the following line when Apollo Client is configured:
- *
- *
- *
- * Below in your <App />, wrap your pages in an <ApolloProvider /> component
- * and pass it `client` as the `client` prop value so they will
- * have access to data exposed by your GraphQL API.
- */
-
-/**
- * @TODO: Add Routing
- *
- * Uncomment the following line when your routes are configured
- *
- *
- *
- * Below in your <App />, nest your <Routes /> inside of <BrowserRouter />
- * component to enable routing in your client app.
- */
-
-/**
- * @TODO: Initialize Redux Store
- *
- * Uncomment the following line when your Redux store is configured
- *
- *
- *
- * Below in your <App />, wrap a <ReduxProvider /> component around all
- * of the app's children, and pass it the imported `store` as the `store`
- * prop's value.
- */
-
-/**
- * @TODO: Add the Viewer Context
- *
- * import { ViewerProvider } from './context/ViewerProvider'
- *
- * Below in your <App />, wrap the <ViewerProvider /> component around
- * the <BrowserRouter /> component so the router is aware of whether a
- * user is currently logged in and who that user is.
- */
-
-// @TODO: Remove this import once you have your router working below
+import { ViewerProvider } from './context/ViewerProvider';
 import Home from './pages/Home';
 import ItemsGrid from './components/ItemsGrid';
 import store from './redux';
@@ -73,9 +28,11 @@ const App = () => {
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
         <ApolloProvider client={client}>
-          <BrowserRouter>
-            <Layout />
-          </BrowserRouter>
+          <ViewerProvider>
+            <BrowserRouter>
+              <Layout />
+            </BrowserRouter>
+          </ViewerProvider>
         </ApolloProvider>
       </MuiThemeProvider>
     </ReduxProvider>

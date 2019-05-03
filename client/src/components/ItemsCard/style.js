7a8dc9ef2716bf16b247f4d829fde5a5ac89d3e1
Gavin Jin <gavinjin62@gmail.com>
2019-05-02 21:37:09 -0700

    add top margin

--- client/src/components/ItemsCard/style.js
+++ client/src/components/ItemsCard/style.js
@@ -2,7 +2,8 @@ const styles = theme => ({
   card: {
     height: '100%',
     display: 'flex',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    marginTop: '50px'
   },
   cardMedia: {
     paddingTop: '56.25%' // 16:9

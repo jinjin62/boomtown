42413c58cbd0d1e69ccc2c51e764f2e4da2c4dfd
Gavin Jin <gavinjin62@gmail.com>
2019-05-05 20:53:48 -0700

    fix item query imistake

--- client/src/apollo/queries.js
+++ client/src/apollo/queries.js
@@ -1,9 +1,5 @@
 import gql from 'graphql-tag';
 
-/**
- * Item and user-related queries and mutations.
- */
-
 const ItemFields = gql`
   fragment ItemFields on Item {
     id
@@ -29,14 +25,14 @@ const ItemFields = gql`
     }
   }
 `;
-// export const ITEM_QUERY = gql`
-//   query item($id: ID!) {
-//     items(filter: $id) {
-//       ...ItemFields
-//     }
-//   }
-//   ${ItemFields}
-// `;
+
+export const ITEM_QUERY = gql`
+  query item($id: ID!) {
+    items(filter: $id)
+    ...ItemFields
+  }
+  ${ItemFields}
+`;
 
 export const ALL_ITEMS_QUERY = gql`
   query items($filter: ID) {
@@ -49,14 +45,16 @@ export const ALL_ITEMS_QUERY = gql`
 
 export const ALL_USER_ITEMS_QUERY = gql`
   query user($id: ID!) {
-    bio
-    email
-    fullname
-    items {
-      ...ItemFields
-    }
-    borrowed {
-      ...ItemFields
+    user(id: $id) {
+      bio
+      email
+      fullname
+      items {
+        ...ItemFields
+      }
+      borrowed {
+        ...ItemFields
+      }
     }
   }
   ${ItemFields}
@@ -70,42 +68,6 @@ export const ALL_TAGS_QUERY = gql`
     }
   }
 `;
-
-// export const ADD_ITEM_MUTATION = gql`
-//   mutation addItem($item: NewItemInput!) {
-//     # @TODO: Pass the item and image into the addItem mutation as arguments
-//     # and return the new item id when the mutation is complete.
-//   }
-// `;
-
-/**
- * Auth-related queries and mutations.
- */
-
-// export const VIEWER_QUERY = gql`
-//   query {
-//     # @TODO: Query the id, email, fullname, and bio fields for the viewer.
-//   }
-// `;
-// export const LOGOUT_MUTATION = gql`
-//   mutation {
-//     # @TODO: Run the logout mutation.
-//   }
-// `;
-
-// export const SIGNUP_MUTATION = gql`
-//   mutation signup($user: SignupInput!) {
-//     # @TODO: Pass the user into the signup mutation as an argument
-//     # and return the id of the new user when the mutation is complete.
-//   }
-// `;
-
-// export const LOGIN_MUTATION = gql`
-//   mutation login($user: LoginInput!) {
-//     # @TODO: Pass the user into the login mutation as an argument
-//     # and return the id of the new user when the mutation is complete.
-//   }
-// `;
 
 export const ADD_ITEM_MUTATION = gql`
   mutation addItem($item: NewItemInput!) {
@@ -122,33 +84,29 @@ export const ADD_ITEM_MUTATION = gql`
 `;
 
 export const VIEWER_QUERY = gql`
-  query {
+  query viewerQuery {
     viewer {
       id
       email
       fullname
-      bio
     }
   }
 `;
+
 export const LOGOUT_MUTATION = gql`
-  mutation {
+  mutation logout {
     logout
   }
 `;
 
 export const SIGNUP_MUTATION = gql`
   mutation signup($user: SignupInput!) {
-    signup(user: $user) {
-      id
-    }
+    signup(user: $user)
   }
 `;
 
 export const LOGIN_MUTATION = gql`
   mutation login($user: LoginInput!) {
-    login(user: $user) {
-      id
-    }
+    login(user: $user)
   }
 `;

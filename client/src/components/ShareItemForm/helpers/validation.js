042be8049ae1a27a6b4668731b77b91e2e89c0bd
Gavin Jin <gavinjin62@gmail.com>
2019-05-06 02:55:57 -0700

    add form validation to itemshareform

--- client/src/components/ShareItemForm/helpers/validation.js
+++ client/src/components/ShareItemForm/helpers/validation.js
@@ -1,11 +1,18 @@
 export default function validate(values) {
   const errors = {};
 
-  /**
-   * @TODO: Write the validation rules for the share form.
-   *
-   * An item title, description, and at least one tag is required for all items.
-   */
+  if (values.title === '') {
+    errors.title = 'Title is missing';
+  }
+  if (values.description === '') {
+    errors.description = 'Description is missing';
+  }
+  if (!fileSelected) {
+    errors.fileSelected = 'Image is missing';
+  }
+  if (!selectedTags) {
+    errors.tag = 'At least one tag must be selected';
+  }
 
   return errors;
 }
